0871f63bef84cc59f2b623009c466f22cb2f552f
Muflone <webreg@vbsimple.net>
2018-11-11 14:20:02 +0100

    Made some company fields optional

--- hotels/models/company.py
+++ hotels/models/company.py
@@ -3,13 +3,16 @@ from django.db import models
 class Company(models.Model):
 
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    address = models.TextField()
-    phone1 = models.CharField(max_length=255)
-    phone2 = models.CharField(max_length=255)
-    fax = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    address = models.TextField(blank=True)
+    phone1 = models.CharField(max_length=255, blank=True)
+    phone2 = models.CharField(max_length=255, blank=True)
+    fax = models.CharField(max_length=255, blank=True)
+    email = models.CharField(max_length=255, blank=True)
 
     class Meta:
         # Define the database table
         db_table = 'companies'
+
+    def __str__(self):
+        return self.name
