4497e34a0ff57d03a3186b09166728b40cfce7b6
Muflone <webreg@vbsimple.net>
2018-11-11 15:53:41 +0100

    Added floor description

--- hotels/models/floor.py
+++ hotels/models/floor.py
@@ -3,6 +3,7 @@ from django.db import models
 class Floor(models.Model):
 
     name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True)
 
     class Meta:
         # Define the database table
