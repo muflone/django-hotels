ca629eb369fef2a0fe4b3c28a26fc758902a9f45
Muflone <webreg@vbsimple.net>
2018-11-18 17:58:50 +0100

    Added route and view for home page

--- hotels/views.py
+++ hotels/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from .models import PageSection
+
+
 # Create your views here.
+def home(request):
+    home_section = PageSection.objects.filter(name='Home')[0]
+    header_sections = PageSection.objects.filter(header_order__gt=0)
+    home_sections = PageSection.objects.filter(home_order__gt=0)
+    return render(request, 'hotels/home.html', {
+                    'page_title': home_section.home_title,
+                    'page_content': home_section.description.replace(
+                        '\r', '').split('\n\n'),
+                    'request_path': request.path,
+                    'home_section': home_section,
+                    'header_sections': header_sections.order_by('header_order'),
+                    'home_sections': home_sections.order_by('home_order'),
+                  })
