65142078abf66f0c7edf56d256018ad89ce3f7e5
Muflone <webreg@vbsimple.net>
2018-12-03 00:50:47 +0100

    Moved AdminTextInputFilter into utility application

--- utility/admin.py
+++ utility/admin.py
@@ -20,4 +20,20 @@
 
 from django.contrib import admin
 
-# Register your models here.
+
+class AdminTextInputFilter(admin.SimpleListFilter):
+    template = 'utility/admin_text_input_filter/form.html'
+
+    def lookups(self, request, model_admin):
+        # Dummy, required to show the filter.
+        return (('', ''),)
+
+    def choices(self, changelist):
+        # Grab only the "all" option.
+        all_choice = next(super().choices(changelist))
+        all_choice['query_parts'] = (
+            (k, v)
+            for k, v in changelist.get_filters_params().items()
+            if k != self.parameter_name
+        )
+        yield all_choice
