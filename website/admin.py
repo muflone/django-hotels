d44354aef4a6117aa48c0c794d9924933e30af70
Muflone <webreg@vbsimple.net>
2019-02-03 03:27:13 +0100

    Customize models list_display

--- website/admin.py
+++ website/admin.py
@@ -36,6 +36,8 @@ admin.site.register(AdminSearchable, AdminSearchableAdmin)
 admin.site.register(AdminSection, AdminSectionAdmin)
 admin.site.register(HomeSection, HomeSectionAdmin)
 
+admin_models = get_admin_models()
+
 # Customize Administration
 try:
     for section in AdminSection.objects.all():
@@ -49,8 +51,6 @@ except OperationalError:
     # If the model AdminSection doesn't yet exist skip the customization
     pass
 
-# Customize models
-admin_models = get_admin_models()
 # Customize searchables
 try:
     for item in AdminSearchable.objects.filter(use_select2=True):
@@ -59,3 +59,26 @@ try:
 except OperationalError:
     # If the model AdminSearchable doesn't yet exist skip the customization
     pass
+
+# Customize list_display
+try:
+    # Clear or initialize the model list_display
+    for model_name in admin_models:
+        admin_models[model_name].list_display = []
+    # Add the fields to model list_display
+    for item in AdminListDisplay.objects.filter(enabled=True).order_by(
+            'model', 'order'):
+        admin_models[item.model].list_display.append(item.field)
+except OperationalError:
+    # If the model AdminListDisplay doesn't yet exist skip the customization
+    pass
+
+# Final checks on every model
+for model_name in admin_models:
+    # Add missing fields from model list_display_links to model list_display
+    for item in admin_models[model_name].list_display_links:
+        if item not in admin_models[model_name].list_display:
+            admin_models[model_name].list_display.append(item)
+    # For empty model list_display add the standard __str__ field
+    if not admin_models[model_name].list_display:
+        admin_models[model_name].list_display.append('__str__')
